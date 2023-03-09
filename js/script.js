de6537311b8de2c6c686f8e92d32951a9bcf26cf
Pawel Rzeznicki <p.rzeznicky@gmail.com>
2023-03-09 21:49:55 +0100

    Add object tag list with numbers in left sidebar

--- js/script.js
+++ js/script.js
@@ -72,8 +72,8 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  /* [DONE] create a new variable allTags with an empty object */
+  let allTags = {};
   // console.log(allTags);
   /* [DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
@@ -100,9 +100,12 @@ function generateTags(){
       html = html + linkHTML + ' ';
       // console.log('linkHTML: v2: ', linkHTML);
       /* [NEW] check if this link is NOT already in allTags */
-      if(allTags.indexOf(linkHTML) == -1){
+      if(!allTags.hasOwnProperty(tag)){
         /* [NEW] add generated code to allTags array */
-        allTags.push(linkHTML);
+        allTags[tag]=1;
+      }
+      else {
+        allTags[tag]++;
       }
     /* [DONE] END LOOP: for each tag */
     }
@@ -113,11 +116,18 @@ function generateTags(){
   }
   /* [NEW] find list of tags in right column */
   const tagList = document.querySelector(optTagsListSelector);
-  console.log('tagList: ', tagList);
-  console.log('allTags: ', allTags);
-  /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
-  console.log('tagList updated: ', tagList);
+
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+  /* [NEW] START LOOP: for each tag in allTags */
+  for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ')</a></li>';
+    // ' (' + allTags[tag] + ')
+  /* [NEW] END LOOP: for each tag in allTags */
+  }
+  /* [NEW] add html from allTagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
 }
 generateTags();
 
